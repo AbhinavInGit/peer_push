fc24b44ff1e3b2c7179ddf3ec76c66213633f2cd
ishatiwari21 <156085572+ishatiwari21@users.noreply.github.com>
2025-04-15 00:34:47 +0530

    Update landing.js

--- landing/landing.js
+++ landing/landing.js
@@ -6,7 +6,7 @@ addBtn.addEventListener('click', function (event) {
   const streakValue = parseInt(streakInput.value);
   if (streakValue > 30) {
     errorMsg.style.display = 'block';
-    event.preventDefault(); // Stops navigation
+    event.preventDefault(); 
   } else {
     errorMsg.style.display = 'none';
   }
